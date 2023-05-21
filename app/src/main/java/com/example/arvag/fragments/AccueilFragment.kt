a3b928ba2d144cc63a79d787f584e3255b56ee03
Henya <henya.zahiri@imt-atlantique.net>
2023-05-21 11:41:05 +0200

    Accueil bouton site et avancée projet

--- app/src/main/java/com/example/arvag/fragments/AccueilFragment.kt
+++ app/src/main/java/com/example/arvag/fragments/AccueilFragment.kt
@@ -1,62 +1,40 @@
 package com.example.arvag.fragments
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
 import com.example.arvag.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-//comment
-/**
- * A simple [Fragment] subclass.
- * Use the [AccueilFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class AccueilFragment : Fragment() {
-    /*fun openWebPage(view: View?) {
-        val webpage: Uri = Uri.parse("https://www.arvag.org")
-        val intent = Intent(Intent.ACTION_VIEW, webpage)
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent)
-        }
-    }*/
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    lateinit var moreInfoButton: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
+        val view = inflater.inflate(R.layout.fragment_accueil, container, false)
+        return view
+    }
+
+    fun Bundle?.onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_accueil, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AcceuilFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AccueilFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        moreInfoButton = view.findViewById(R.id.buttonsite)
+        moreInfoButton.setOnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.arvag.org/"))
+            startActivity(browserIntent)
+        }
     }
-}
+    }
